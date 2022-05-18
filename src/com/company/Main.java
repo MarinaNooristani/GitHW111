42eb2f599532c99616382fa290c41e64aeb74c03
MarinaNooristani <marina.nooristani321@gmail.com>
2022-05-18 20:05:11 +0300

    numbers from 1 to 50

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // add code in it that prints the numbers from 1 to 100
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 50; i++) {
             System.out.print(i + " ");
         }
     }
