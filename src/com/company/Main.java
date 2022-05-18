c8b217b6b4616a89e78918fc049c1f34465bbf09
MarinaNooristani <marina.nooristani321@gmail.com>
2022-05-18 19:57:02 +0300

    numbers from 1 to 100

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // add code in it that prints the numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
